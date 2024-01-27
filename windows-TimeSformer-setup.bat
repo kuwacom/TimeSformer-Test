461214ebad2f81239bd73a184857369bbae993dc
kuwacom <kuwa.com3@gmail.com>
2024-01-27 23:57:07 +0900

    remove: 対応しないファイルを削除

--- windows-TimeSformer-setup.bat
+++
@@ -1,9 +0,0 @@
-.\venv\Scripts\activate
-@REM TimeSformerをclone
-git clone https://github.com/facebookresearch/TimeSformer
-cd TimeSformer
-py setup.py build develop
-cd ../
-
-@REM create model file
-mkdir models
